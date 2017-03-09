09b4025949f67b2526f8d4627de7cc462e613659
Alex Tang <alex.m.tang@gmail.com>
2017-03-09 17:17:18 -0500

    implement search

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,11 @@ module.exports = function makeRouterWithSockets (io) {
 
   // a reusable function
   function respondWithAllTweets (req, res, next){
-    // var allTheTweets = tweetBank.list();
-
-    db.query('select tweets.id as id, users.name as name, tweets.content as text from users join tweets on users.id = tweets.user_id', function (err, result) {
+    var dbQuery = 'select tweets.id as id, users.name as name, tweets.content as text from users join tweets on users.id = tweets.user_id';
+    if (req.query.search) {
+      dbQuery = `select tweets.id as id, users.name as name, tweets.content as text from users join tweets on users.id = tweets.user_id where tweets.content LIKE '%${req.query.search}%'`;
+    }
+    db.query(dbQuery, function (err, result) {
       if (err) return next(err); // pass errors to Express
       var tweets = result.rows.reverse();
       res.render('index', {
@@ -19,7 +21,6 @@ module.exports = function makeRouterWithSockets (io) {
         tweets: tweets,
         showForm: true
       });
-      // return tweets;
     });
   }
 
@@ -63,20 +64,23 @@ module.exports = function makeRouterWithSockets (io) {
         name: req.body.name,
         text: tweet.content
       };
-      console.log(result)
-      // var tweets = result.rows;
-
       io.sockets.emit('new_tweet', newTweet);
       res.redirect('/');
-      // res.render('index', {
-      //   title: 'Twitter.js',
-      //   tweets: tweets
-      // });
     });
-    // var newTweet = tweetBank.add(req.body.name, req.body.text);
-    // io.sockets.emit('new_tweet', newTweet);
-    // res.redirect('/');
   });
+
+  // router.get('/tweets', function(req, res, next){
+  //   db.query('select tweets.id as id, users.name as name, tweets.content as text from users join tweets on users.id = tweets.user_id and tweets.content LIKE %$1%', [req.query.search], function (err, result) {
+  //     if (err) return next(err); // pass errors to Express
+  //     var tweets = result.rows;
+  //     console.log('query', req.query);
+  //     console.log('tweets', tweets);
+  //     res.render('index', {
+  //       title: 'Twitter.js',
+  //       tweets: tweets
+  //     });
+  //   });
+  // });
 
   // // replaced this hard-coded route with general static routing in app.js
   // router.get('/stylesheets/style.css', function(req, res, next){
